d680519d513d7c93788f99ceb88a38bcd14bcd13
morifurry <117458581+morifurry@users.noreply.github.com>
2024-02-11 16:57:27 +0800

    Add files via upload

---
+++ omni_x20960_5m_wxga_v1.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from nikel device
+$(call inherit-product, device/kte/x20960_5m_wxga_v1/device.mk)
+
+PRODUCT_DEVICE := x20960_5m_wxga_v1
+PRODUCT_NAME := omni_x20960_5m_wxga_v1
+PRODUCT_BRAND := kte
+PRODUCT_MODEL := x20l
+PRODUCT_MANUFACTURER := kte
+
+PRODUCT_GMS_CLIENTID_BASE := android-kte
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="nikel-user 6.0 MRA58K V10.2.1.0.MBFMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/nikel/nikel:6.0/MRA58K/V10.2.1.0.MBFMIXM:user/test-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_x20960_5m_wxga_v1-user
+add_lunch_combo omni_x20960_5m_wxga_v1-userdebug
+add_lunch_combo omni_x20960_5m_wxga_v1-eng
